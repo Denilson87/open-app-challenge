c161fb6b70c35ae46104d6f36372fb657cab7bed
Denilson Mucavele <denilsonmucavel@outlook.com>
2022-04-03 00:31:23 +0200

    getting the view Id

--- app/src/main/java/com/example/openmrs_app/FormLogin.java
+++ app/src/main/java/com/example/openmrs_app/FormLogin.java
@@ -2,14 +2,19 @@ package com.example.openmrs_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class FormLogin extends AppCompatActivity {
 
+    private TextView text_tela_cadastro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_login);
 
         getSupportActionBar().hide();
+    }
+    private void IniciarComponents(){
+        text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
     }
 }
