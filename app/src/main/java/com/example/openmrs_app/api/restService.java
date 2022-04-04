ef212b70faa0852477e62bf5c04380439b2f180c
Denilson Mucavele <denilsonmucavel@outlook.com>
2022-04-04 23:08:27 +0200

    updating interface

--- app/src/main/java/com/example/openmrs_app/api/restService.java
+++ app/src/main/java/com/example/openmrs_app/api/restService.java
@@ -1,9 +1,13 @@
 package com.example.openmrs_app.api;
-
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface restService {
 
-    @
+        //consultar patient no webservice openMRS
+        @GET("{result}/json/")
+        Call<patient>searchPatient(@Path("/patient?q=Marcos&v=default&limit=7") String patient);
 }
 
 
