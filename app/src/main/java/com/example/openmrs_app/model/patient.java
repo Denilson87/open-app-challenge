2b45da65df99366ac15a3fdd7e8d242a0cfa06a7
Denilson Mucavele <denilsonmucavel@outlook.com>
2022-04-04 22:47:35 +0200

    updating models

---
+++ app/src/main/java/com/example/openmrs_app/model/patient.java
@@ -0,0 +1,51 @@
+package com.example.openmrs_app.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class patient {
+    @SerializedName("display")
+    @Expose
+    private String display;
+    @SerializedName("birthdate")
+    @Expose
+    private String birthdate;
+    @SerializedName("gender")
+    @Expose
+    private String gender;
+    @SerializedName("attributes")
+    @Expose
+    private String attributes;
+
+    public String getDisplay() {
+        return display;
+    }
+
+    public void setDisplay(String display) {
+        this.display = display;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(String attributes) {
+        this.attributes = attributes;
+    }
+}
